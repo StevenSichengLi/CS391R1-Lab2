5109acfe97d89c72b1e39980933fae66bfbc8468
Sicheng Li <lsich@cs730-test8.bu.edu>
2025-10-02 13:39:40 -0400

    2.1 finished, need to make more changes to tb

---
+++ lab2.srcs/sim_1/new/light_controller_tb.sv
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/01/2025 01:28:44 PM
+// Design Name: 
+// Module Name: light_controller_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module light_controller_tb;
+    reg clk, rst, button;           
+    wire [2:0] light_state;         
+
+    parameter N = 3;
+
+    light_controller #(N) uut (
+        .clk(clk),
+        .rst(rst),
+        .button(button),
+        .light_state(light_state)
+    );
+
+    // 10 ns clock period
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0; rst = 1; button = 0;
+
+        // release rst after clock cycles
+        repeat(2) @(posedge clk); rst = 0;
+
+        // Press button
+        button = 1;
+        repeat(12) @(posedge clk);  // hold enough cycles to see cycling
+
+        // Release button we should see it turn off
+        button = 0;
+        repeat(5) @(posedge clk);
+
+        // Press again
+        button = 1;
+        repeat(8) @(posedge clk);
+        button = 0;
+
+        #20 $finish;
+    end
+
+    initial begin
+        $monitor("t=%0t  clk=%b  btn=%b  light_state=%0d",
+                 $time, clk, button, light_state);
+    end
+endmodule 
---
+++ lab2.srcs/sources_1/new/light_controller.sv
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/01/2025 12:10:09 PM
+// Design Name: 
+// Module Name: light_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module light_controller #(parameter N = 10)(
+    input  wire clk, rst, button,
+    output reg  [2:0] light_state
+    );
+    
+    // indicator value, if button pressed before, then it is 1 else 0
+    reg button_prev;
+    
+    // large enough for big N   
+    reg [31:0] count;          
+
+    // run if clock is incremented or button is reset
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            button_prev <= 1'b0;
+            light_state <= 3'd0;
+            count       <= 0;
+        end
+        else begin
+            // if the button is pressed the frist time
+            if (button && !button_prev) begin
+                // turn light on (color 1)
+                light_state <= 3'd1;
+                count       <= 0;
+            end
+
+            // else if the button is held down
+            else if (button) begin
+                // if we are at the end of the cycle
+                if (count == N-1) begin
+                    count <= 0;
+                    // if we are at the end of the light cycle, we wrap around
+                    if (light_state == 3'd7)
+                        light_state <= 3'd1;
+                    // else we increment
+                    else
+                        light_state <= light_state + 3'd1;
+                end
+                // if we haven't reached the time limit
+                else begin
+                    count <= count + 1;
+                end
+            end
+
+            // if we release the button
+            else begin
+                // reset everything and turn the light off
+                light_state <= 3'd0;
+                count       <= 0;
+            end
+
+            // Update previous button status
+            button_prev <= button;
+        end
+    end
+endmodule
+
